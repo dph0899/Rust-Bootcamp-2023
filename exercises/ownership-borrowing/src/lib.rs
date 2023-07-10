f69478fb821910818ea18e13657ae65c12b0298e
Pham Duong <dph0899@gmail.com>
2023-07-11 03:24:00 +0700

    giai bai tap buoi 2

--- exercises/ownership-borrowing/src/lib.rs
+++ exercises/ownership-borrowing/src/lib.rs
@@ -1,10 +1,24 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
 // Exercise 1
 // Make it compile
-fn exercise1() {
-    // Use as many approaches as you can to make it work
+// Cach 1
+fn exercise1a() {
+    let x = String::from("hello, world");
+    let y = x.clone();
+    let z = x.clone();
+}
+// Cach 2
+fn exercise1b() {
+    let x = String::from("hello, world");
+    let y = &x;
+    let z = &x;
+}
+// Cach 3
+fn exercise1c() {
     let x = String::from("hello, world");
     let y = x;
-    let z = x;
+    let z = y;
 }
 
 // Exercise 2
@@ -17,13 +31,11 @@ fn exercise2() {
     println!("{}", s2);
 }
 // Only modify the code below!
-fn take_ownership(s: String) {
+fn take_ownership(s: String) -> String {
     println!("{}", s);
+    s
 }
 
-// Exercise 3
-// Make it compile
-// Dont care about logic
 fn exercise3() {
     let values: Vec<f64> = vec![
         2817.42, 2162.17, 3756.57, 2817.42, -2817.42, 946.9, 2817.42, 964.42, 795.43, 3756.57,
@@ -37,23 +49,22 @@ fn exercise3() {
 
     println!("{:?}", values_number);
 
-    while additions.len() > 0 {
+    while !additions.is_empty() {
         let mut addition: f64 = 0.0;
-
-        // Sumar valores en additions
-        for element_index in additions {
+        for &element_index in &additions {
             let addition_aux = values[element_index];
             addition = addition_aux + addition;
         }
+
+        println!("{}", addition);
     }
 }
 
 // Exercise 4
 // Make it compile
-fn exercise4(value: u32) -> &'static str {
+fn exercise4(value: u32) -> String {
     let str_value = value.to_string(); // Convert u32 to String
-    let str_ref: &str = &str_value; // Obtain a reference to the String
-    str_ref // Return the reference to the String
+    str_value
 }
 
 // Exercise 5
@@ -65,11 +76,11 @@ fn exercise5() {
     let key = 3;
 
     let res = match my_map.get(&key) {
-        Some(child) => child,
+        Some(child) => child.clone(),
         None => {
             let value = "3.0".to_string();
-            my_map.insert(key, value);
-            &value // HERE IT FAILS
+            my_map.insert(key, value.clone());
+            value
         }
     };
 
@@ -79,17 +90,28 @@ fn exercise5() {
 // Exercise 6
 // Make it compile
 
-use std::io;
+use std::io::{self, BufRead};
 
 fn exercise6() {
-    let mut prev_key: &str = "";
+    let mut prev_key: String = String::new();
 
-    for line in io::stdin().lines() {
-        let s = line.unwrap();
+    let stdin = io::stdin();
 
-        let data: Vec<&str> = s.split("\t").collect();
-        if prev_key.len() == 0 {
-            prev_key = data[0];
+    loop {
+        let mut line = String::new();
+        match stdin.read_line(&mut line) {
+            Ok(_) => {
+                let s = line.trim();
+
+                let data: Vec<&str> = s.split('\t').collect();
+                if prev_key.is_empty() {
+                    prev_key = data[0].to_string();
+                }
+            }
+            Err(error) => {
+                eprintln!("Error reading input: {}", error);
+                break;
+            }
         }
     }
 }
@@ -97,35 +119,36 @@ fn exercise6() {
 // Exercise 7
 // Make it compile
 fn exercise7() {
-    let mut v: Vec<&str> = Vec::new();
+    let mut v: Vec<String> = Vec::new();
     {
         let chars = [b'x', b'y', b'z'];
-        let s: &str = std::str::from_utf8(&chars).unwrap();
-        v.push(&s);
+        let s: String = String::from_utf8(chars.to_vec()).unwrap();
+        v.push(s);
     }
     println!("{:?}", v);
 }
 
+
 // Exercise 8
 // Make it compile
 fn exercise8() {
-    let mut accounting = vec!["Alice", "Ben"];
-    
-    loop {
-        let mut add_input = String::from("");
+    let mut accounting = vec!["Alice".to_string(), "Ben".to_string()];
 
-        io::stdin()
-            .read_line(&mut add_input)
-            .expect("Failed to read line");
+    let stdin = io::stdin();
+    let lines = stdin.lock().lines();
 
-        let add_vec: Vec<&str> = add_input.trim()[..].split_whitespace().collect();
+    for line_result in lines {
+        let line = line_result.expect("Failed to read line");
+        let add_input = line.trim().to_string();
+        let add_vec: Vec<&str> = add_input.split_whitespace().collect();
 
-        if add_vec.len() < 1 {
+        if add_vec.is_empty() {
             println!("Incorrect input, try again");
             continue;
         }
 
-        let person = add_vec[0];
+        let person = add_vec[0].to_string();
         accounting.push(person);
     }
 }
+
